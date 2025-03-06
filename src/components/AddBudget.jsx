5a884fffd1bca35ed88d39c04f860af823ee37ea
noorinoorullah130 <noorinoorullah130@gmail.com>
2025-03-06 14:05:28 +0430

    handlechange function added

--- src/components/AddBudget.jsx
+++ src/components/AddBudget.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddBudget = () => {
+    const [budget, setBudget] = useState("");
+
+    const handleChange = (e) => {
+        setBudget(e.target.value);
+    };
+
     return (
         <div className="add-budget">
             <h1>Add Budget</h1>
             <label htmlFor="budget">Budget:</label>
-            <input type="number" id="budget" placeholder="Enter you budget" />
+            <input
+                type="number"
+                value={budget}
+                onChange={handleChange}
+                id="budget"
+                placeholder="Enter you budget"
+                min={0}
+            />
             <button>Add Budget</button>
         </div>
     );
